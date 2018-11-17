d4592dcbd0d6b67cdf2774e67ec947056a0418db
jameer pathan <jameerpathan111@gmail.com>
2018-11-17 15:36:41 +0530

    c2f.rb

---
+++ c2f.rb
@@ -0,0 +1,11 @@
+#Simple, limited-purpose Celsius-to -Fahrenheit converter
+celsius = 100
+fahrenheit = (celsius * 9 / 5) + 32
+#display output using puts
+puts "The result is"
+puts  fahrenheit
+puts "."
+ # display output using print
+print "The result is "
+print fahrenheit
+print ".\n" #use \n for new line
